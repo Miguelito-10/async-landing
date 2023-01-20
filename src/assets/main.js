734d0eaffd2720ca6e51ac436995ef528416132c
Miguel <maickys123@gmail.com>
2023-01-20 15:30:03 -0500

    project complete

--- src/assets/main.js
+++ src/assets/main.js
@@ -24,19 +24,16 @@ async function fetchData(urlApi) {
        <div class="group relative">
          <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-           <p class="w-full">${covids.response.population} ${covids.cases.new} </p>
+          <p> country: ${covids.country}</p>
+          <p class="w-full"> population: ${covids.population} </p>
+          <p >New Cases: ${covids.cases.new} </p>
+          <p class="w-full">Total deaths: ${covids.deaths.total}</p>
          </div>
-         <div class="mt-4 flex justify-between">
-          <h3 class="text-sm text-gray-700">
-             <span aria-hidden="true" class="absolute inset-0"></span>
-             ${covids.response.deaths.total}
-           </h3>
          </div>
        </div>
-     `).slice(0,4).join('')}
+     `).slice(0,99).join('')}
      `;
          content.innerHTML = view;
-        
      } catch (error) {
          console.error(error)
      }
